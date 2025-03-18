d08916505f550df2b23f1f9599c362569729e996
Roope Kotiniemi <kotiniemiroope@gmail.com>
2025-03-18 22:11:57 +0200

    F

---
+++ Crazy/Crazy2/Crazy2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Crazy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a < 50; a--)
+            {
+                Console.WriteLine("Mutsis{0} ", a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
