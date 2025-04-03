e7cf81d09499425446bffd98fd72b043117b407c
Kotiniemi Roope <gr302602@gradia.fi>
2025-04-03 09:16:49 +0300

    Palindromi tarkistaja

---
+++ PalindromeChecker/PalindromeChecker/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PalindromeChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä Sana: ");
+            string? input = Console.ReadLine();
+            if (input == null)
+            {
+                Console.WriteLine("Empty.");
+                return;
+            }
+            string str = input;
+            bool isPalindrome = true;
+            int length = str.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (str[i] != str[length - i - 1])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome)
+            {
+                Console.WriteLine($"{str} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{str} is not a palindrome.");
+            }
+        }
+    }
+}
